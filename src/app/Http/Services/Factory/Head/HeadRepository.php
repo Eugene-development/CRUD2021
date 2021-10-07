82bf9c638f1f46a7e7a8d072b8687f9055559e77
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-07 20:24:25 +0300

    git init

--- src/app/Http/Services/Factory/Head/HeadRepository.php
+++ src/app/Http/Services/Factory/Head/HeadRepository.php
@@ -10,25 +10,4 @@ use App\Http\Services\Factory\Repository;
 class HeadRepository extends Repository implements Head
 {
     protected $path = 'Factory\Head';
-    protected $serviceORM;
-    protected $serviceOne;
-    protected $serviceAll;
-    protected $serviceWhere;
-    protected $serviceSearch;
-    protected $serviceCreate;
-    protected $serviceUpdate;
-    protected $serviceDelete;
-
-    public function __construct()
-    {
-        $this->serviceORM    = app('App\Http\Services\\' . $this->path . '\ServiceORM');
-        $this->serviceOne    = app('App\Http\Services\\' . $this->path . '\ServiceOne');
-        $this->serviceAll    = app('App\Http\Services\\' . $this->path . '\ServiceAll');
-        $this->serviceWhere  = app('App\Http\Services\\' . $this->path . '\ServiceWhere');
-        $this->serviceSearch = app('App\Http\Services\\' . $this->path . '\ServiceSearch');
-        $this->serviceCreate = app('App\Http\Services\\' . $this->path . '\ServiceCreate');
-        $this->serviceUpdate = app('App\Http\Services\\' . $this->path . '\ServiceUpdate');
-        $this->serviceDelete = app('App\Http\Services\\' . $this->path . '\ServiceDelete');
-    }
-
 }
