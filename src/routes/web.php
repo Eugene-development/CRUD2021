9fbdd57e9f9e8311bc9d0019ab2e33043022d0e5
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-07 20:41:19 +0300

    git init

--- src/routes/web.php
+++ src/routes/web.php
@@ -44,3 +44,108 @@ Route::get('/get-all-seo/', [SeoController::class, 'getAll']);
 Route::get('/get-all-text/', [TextController::class, 'getAll']);
 
 
+/*
+|--------------------------------------------------------------------------
+| add
+|--------------------------------------------------------------------------
+|
+| Добавление
+|
+*/
+Route::post('/add-menu', [MenuController::class, 'add']);
+Route::post('/add-head', [HeadController::class, 'add']);
+Route::post('/add-rubric', [RubricController::class, 'add']);
+Route::post('/add-category', [CategoryController::class, 'add']);
+Route::post('/add-product', [ProductController::class, 'add']);
+Route::post('/add-seo', [SeoController::class, 'add']);
+Route::post('/add-text', [TextController::class, 'add']);
+
+
+/*
+|--------------------------------------------------------------------------
+| update
+|--------------------------------------------------------------------------
+|
+| Обновление
+|
+*/
+Route::put('/update-menu', [MenuController::class, 'update']);
+Route::put('/update-head', [HeadController::class, 'update']);
+Route::put('/update-rubric', [RubricController::class, 'update']);
+Route::put('/update-category', [CategoryController::class, 'update']);
+Route::put('/update-product', [ProductController::class, 'update']);
+Route::put('/update-seo', [SeoController::class, 'update']);
+Route::put('/update-text', [TextController::class, 'update']);
+
+
+/*
+|--------------------------------------------------------------------------
+| delete
+|--------------------------------------------------------------------------
+|
+| Удаление
+|
+*/
+Route::delete('/delete-menu/{id}', [MenuController::class, 'delete']);
+Route::delete('/delete-head/{id}', [HeadController::class, 'delete']);
+Route::delete('/delete-rubric/{id}', [RubricController::class, 'delete']);
+Route::delete('/delete-category/{id}', [CategoryController::class, 'delete']);
+Route::delete('/delete-product/{id}', [ProductController::class, 'delete']);
+Route::delete('/delete-seo/{id}', [SeoController::class, 'delete']);
+Route::delete('/delete-text/{id}', [TextController::class, 'delete']);
+
+
+/*
+|--------------------------------------------------------------------------
+| ORM
+|--------------------------------------------------------------------------
+|
+| Menu
+|
+*/
+Route::get('/get-menu/{param}', [MenuController::class, 'getMenu']);
+
+
+/*
+|--------------------------------------------------------------------------
+| ORM
+|--------------------------------------------------------------------------
+|
+| Head
+|
+*/
+Route::get('/get-head/{param}', [HeadController::class, 'getHead']);
+
+
+/*
+|--------------------------------------------------------------------------
+| ORM
+|--------------------------------------------------------------------------
+|
+| Rubric
+|
+*/
+Route::get('/get-rubric/{param}', [RubricController::class, 'getRubric']);
+
+
+/*
+|--------------------------------------------------------------------------
+| ORM
+|--------------------------------------------------------------------------
+|
+| Category
+|
+*/
+Route::get('/get-category/{param}', [CategoryController::class, 'getCategory']);
+
+
+/*
+|--------------------------------------------------------------------------
+| ORM
+|--------------------------------------------------------------------------
+|
+| Product
+|
+*/
+Route::get('/get-product/{param}', [ProductController::class, 'getProduct']);
+
